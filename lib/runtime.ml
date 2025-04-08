f3ff7077e97896fed74e1a4247aa97cda97ac4c0
Aleksei Lobanov <2401213370@stu.pku.edu.cn>
2025-04-08 18:04:19 +0800

    runtime: minimal working version

--- lib/runtime.ml
+++ lib/runtime.ml
@@ -1,114 +1,141 @@
-type address = { idx : int }
-type value = Number of int | Address of address | Null
-
 module StringMap = Map.Make (String)
 
+type operand = Number of int | Local of int | Global of int
+type address = Local of int | Global of int
+type target = Constant of int | Dynamic of int
+
 type obj =
-  | Value of value
-  | Object of { addr : address; fields : value StringMap.t }
+  | HeapNumber of int
+  | HeapArray of obj array
+  | HeapObject of { fields : obj StringMap.t }
+
+let rec string_of_obj h o =
+  match o with
+  | HeapNumber x -> string_of_int x
+  | HeapArray a ->
+      Array.fold_left (fun acc o -> acc ^ string_of_obj h o ^ ",") "[" a ^ "]"
+  | HeapObject o ->
+      StringMap.fold
+        (fun f o acc -> Printf.sprintf "%s%s: %s," acc f (string_of_obj h o))
+        o.fields "{"
+      ^ "}"
 
 type command_kind =
+  | Halt
   | Nop
-  | Add
-  | Sub
-  | Push of value
-  | Pop of address
-  | Goto of value
-  | IfZeroGoto of value
-  | SetField of string
-  | GetField of string
-  | Alloc
-  | BuiltinCall of string
+  | Trap
+  | Add of (address * operand * operand)
+  | Sub of (address * operand * operand)
+  | Assign of (address * operand)
+  | Goto of target
+  | GotoIfZero of (operand * target)
+  | Call of (address * operand array * target)
+  | Ret of operand
+  | Alloca of int
+  | Builtin of (address * operand array * string)
 
-type command = { kind : command_kind; loc : Location.location }
+type command = { cmd : command_kind; loc : Location.location }
+type heap = { memory : obj array; free : int }
 
-type runtime = {
-  isp : int;
-  regs : value array;
+type stack_frame = {
+  locals : int array;
+  caller : stack_frame option;
+  ip : int;
+  return : address;
+}
+
+and runtime = {
+  stack : stack_frame;
+  heap : heap;
   code : command array;
-  stack : value list;
-  memory : obj array;
+  stdin : string;
+  stdout : string;
   source : string;
 }
 
-let create s =
+let create source code main =
   let size = Int.shift_left 1 16 in
   {
-    isp = 0;
-    regs = Array.make 16 (Number 0);
-    code = Array.make size { kind = Nop; loc = Location.Spot 0 };
-    stack = [];
-    memory = Array.make size (Value (Number 0));
-    source = s;
+    heap = { memory = Array.make size (HeapNumber 0); free = 0 };
+    stdout = "";
+    stdin = "";
+    stack = { caller = None; ip = main; locals = [||]; return = Local 0 };
+    code;
+    source;
   }
 
-let rec string_of_val r v loc =
-  match v with
-  | Number x -> string_of_int x
-  | Null -> "null"
-  | Address a ->
-      (let o = r.memory.(a.idx) in
-       match o with
-       | Value v -> string_of_val r v loc
-       | Object o ->
-           StringMap.fold
-             (fun fname v acc ->
-               Printf.sprintf "%s%s: %s," acc fname (string_of_val r v loc))
-             o.fields "{")
-      ^ "}"
+let finished r =
+  let isp = r.stack.ip in
+  let cmd = r.code.(isp) in
+  if cmd.cmd = Halt then true else false
 
 let step r =
-  let cmd = r.code.(r.isp) in
-
-  let next r = { r with isp = r.isp + 1 } in
-  let push r v = { r with stack = v :: r.stack } in
-  let pop r =
-    match r.stack with
-    | v :: vs -> ({ r with stack = vs }, v)
-    | _ -> Error.fail_at_spot r.source "Stack underflow" cmd.loc Error.Eval
-  in
-  let pop_int r =
-    let r, a = pop r in
-    match a with
-    | Number x -> (r, x)
-    | _ -> Error.fail_at_spot r.source "Expected a number" cmd.loc Error.Eval
-  in
-  let goto r v =
+  let next r = { r with stack = { r.stack with ip = r.stack.ip + 1 } } in
+  let get_int v =
     match v with
-    | Number x -> { r with isp = x }
-    | Address a -> (
-        let o = r.memory.(a.idx) in
-        match o with
-        | Value (Number x) -> { r with isp = x }
-        | _ -> Error.fail_at_spot r.source "Invalid address" cmd.loc Error.Eval)
-    | _ -> Error.fail_at_spot r.source "Invalid address" cmd.loc Error.Eval
+    | Number x -> x
+    | Local x -> r.stack.locals.(x)
+    | Global x -> ( match r.heap.memory.(x) with HeapNumber x -> x | _ -> x)
+  in
+  let store_int r addr x =
+    match addr with
+    | Local a ->
+        r.stack.locals.(a) <- x;
+        r
+    | Global a ->
+        r.heap.memory.(a) <- HeapNumber x;
+        r
+  in
+  let get_ip addr =
+    match addr with Constant x -> x | Dynamic x -> r.stack.locals.(x)
   in
 
-  match cmd.kind with
+  let cmd = r.code.(r.stack.ip) in
+  match cmd.cmd with
+  | Halt -> r
   | Nop -> next r
-  | Add ->
-      let r, a = pop_int r in
-      let r, b = pop_int r in
-      push r (Number (a + b)) |> next
-  | Sub ->
-      let r, a = pop_int r in
-      let r, b = pop_int r in
-      push r (Number (a - b)) |> next
-  | Push v -> push r v
-  | Pop addr ->
-      let r, v = pop r in
-      r.memory.(addr.idx) <- Value v;
-      next r
-  | Goto addr -> goto r addr
-  | IfZeroGoto addr ->
-      let r, x = pop_int r in
-      if x = 0 then goto r addr else next r
-  | SetField _ -> failwith "Todo"
-  | GetField _ -> failwith "Todo"
-  | Alloc -> failwith "Todo"
-  | BuiltinCall "print" ->
-      let r, v = pop r in
-      string_of_val r v cmd.loc |> print_string;
-      r
-  | BuiltinCall _ ->
-      Error.fail_at_spot r.source "Unknown builtin function" cmd.loc Error.Eval
+  | Trap -> failwith "Trapped!"
+  | Alloca amt ->
+      next
+        {
+          r with
+          stack =
+            {
+              r.stack with
+              locals = Array.append r.stack.locals (Array.make amt 0);
+            };
+        }
+  | Add (dest, lhs, rhs) -> next @@ store_int r dest (get_int lhs + get_int rhs)
+  | Sub (dest, lhs, rhs) -> next @@ store_int r dest (get_int lhs - get_int rhs)
+  | Assign (dest, src) -> next @@ store_int r dest @@ get_int src
+  | Call (dest, args, fn) ->
+      let f = { r.stack with return = dest } in
+      let f' =
+        {
+          locals = Array.map (fun x -> get_int x) args;
+          caller = Some f;
+          return = Local 0;
+          ip = get_ip fn;
+        }
+      in
+      { r with stack = f' }
+  | Ret v ->
+      let f =
+        match r.stack.caller with
+        | Some f -> f
+        | None ->
+            Error.fail_at_spot r.source "Stack underflow" cmd.loc Error.Eval
+      in
+      next @@ store_int { r with stack = f } f.return (get_int v)
+  | Builtin (_, args, "print_int") ->
+      let s =
+        if Array.length args <> 1 then
+          Error.fail_at_spot r.source "Expected 1 argument" cmd.loc Error.Eval
+        else string_of_int (get_int args.(0))
+      in
+      print_endline s;
+      next { r with stdout = r.stdout ^ s ^ "\n" }
+  | GotoIfZero (v, addr) ->
+      if get_int v = 0 then { r with stack = { r.stack with ip = get_ip addr } }
+      else next r
+  | _ -> failwith "Todo"
---
+++ tests/runtime_basic.ml
@@ -0,0 +1,65 @@
+open Alcotest
+open Pkulang
+
+let interpret cmds n =
+  let r =
+    Runtime.create ""
+      (cmds
+      |> List.map (fun c : Runtime.command ->
+             { cmd = c; loc = Location.Spot 0 })
+      |> Array.of_list)
+      0
+  in
+  let rec complete r n =
+    if Runtime.finished r then r
+    else if n = 0 then failwith "Too many steps!"
+    else complete (Runtime.step r) (n - 1)
+  in
+  (complete r n).stdout
+
+let nothing () = check string "Empty" "" (interpret [ Halt ] 10)
+
+let print () =
+  check string "Print int" "10\n"
+    (interpret [ Builtin (Local 0, [| Number 10 |], "print_int"); Halt ] 10);
+  check string "Print int local" "10\n"
+    (interpret
+       [
+         Alloca 1;
+         Assign (Local 0, Number 10);
+         Builtin (Local 0, [| Local 0 |], "print_int");
+         Halt;
+       ]
+       10)
+
+let fib () =
+  check string "Fib(20)" "6765\n"
+    (interpret
+       [
+         (* 0: main *)
+         Alloca 1;
+         Call (Local 0, [| Number 20 |], Constant 4);
+         Builtin (Local 0, [| Local 0 |], "print_int");
+         Halt;
+         (* 4: fib *)
+         Alloca 2;
+         GotoIfZero (Local 0, Constant 13);
+         Sub (Local 1, Local 0, Number 1);
+         GotoIfZero (Local 1, Constant 14);
+         Sub (Local 2, Local 0, Number 2);
+         Call (Local 1, [| Local 1 |], Constant 4);
+         Call (Local 2, [| Local 2 |], Constant 4);
+         Add (Local 1, Local 1, Local 2);
+         Ret (Local 1);
+         Ret (Number 0);
+         Ret (Number 1);
+         Trap;
+       ]
+       400000)
+
+let () =
+  run "Runtime"
+    [
+      ("basic", [ ("nothing", `Quick, nothing); ("print", `Quick, print) ]);
+      ("programs", [ ("fib", `Quick, fib) ]);
+    ]
