a6372d49f42e964ddab849bd6cc6232a13ae31fa
Aleksei Lobanov <2401213370@stu.pku.edu.cn>
2025-04-08 11:03:09 +0800

    REPL: reading files and multiline repl

--- bin/pkulang.ml
+++ bin/pkulang.ml
@@ -1,13 +1,35 @@
 open Pkulang
 
+let read_input () =
+  let rec read_input' acc =
+    let line = read_line () in
+    if String.ends_with ~suffix:"\\" line then
+      read_input'
+      @@ Printf.sprintf "%s%s\n" acc
+           (String.sub line 0 (String.length line - 1))
+    else acc ^ line
+  in
+  read_input' ""
+
+let do_eval src =
+  let toks = Tokenizer.tokenize src in
+  let _, root = Parser.parse_root { src; toks } in
+  print_newline ();
+  Ast.node_to_str root |> print_endline
+
 let () =
-  while true do
-    try
-      let () = Printf.printf "> " in
-      let src = read_line () in
-      let toks = Tokenizer.tokenize src in
-      let _, root = Parser.parse_stmt { src; toks } in
-      print_newline ();
-      Ast.node_to_str root |> print_endline
-    with _ -> ()
-  done
+  let argv = Sys.argv in
+  let argc = Array.length argv in
+  if argc = 1 then
+    while true do
+      try
+        let () = Printf.printf "> " in
+        let src = read_input () in
+        do_eval src
+      with _ -> ()
+    done
+  else if argc = 2 then
+    let file_name = argv.(1) in
+    let chan = In_channel.open_text file_name in
+    In_channel.input_all chan |> do_eval
+  else failwith @@ Printf.sprintf "Usage: %s [file-name]" argv.(0)
